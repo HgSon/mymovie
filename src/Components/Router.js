fc3d3f96ac359d78f924b1729924ecc6088c50c5
hg <hgson.8@gmail.com>
2020-07-31 10:57:40 +0900

    header ~3.4

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -9,9 +9,11 @@ import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Detail from "Routes/Detail";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 
 export default () => (
   <Router>
+    <Header />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" component={TV} />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "Components/App";
-//.env가 자동으로 src 보게해주기때문에 "./Components/App"안해도됨
 
 ReactDOM.render(<App />, document.getElementById("root"));
